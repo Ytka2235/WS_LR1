e28fbfaae11ea08d845b896afafdf5d85bd3f938
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2024-04-03 10:03:31 +0300

    Добавьте файлы проекта.

---
+++ WS_LR1/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double N = Convert.ToDouble(Console.ReadLine());
+        double x = Convert.ToDouble(Console.ReadLine());
+        double result = -(Math.PI/2);
+        for(int i = 0;i<=N;i++)
+        {
+            result += (Math.Pow(-1, i + 1)) / ((2 * i + 1) * Math.Pow(x, 2 * i + 1));
+        }
+        Console.WriteLine(result);
+    }
+}
